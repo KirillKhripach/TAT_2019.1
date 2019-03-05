b664396b8802dc6ea9c63f849a22ea6cc4ab8fef
KirillKhripach <Kirill.Khripach@gmail.com>
2019-03-06 01:54:54 +0300

    add DevTask-1

---
+++ DevTask1/DevTask1/EntryPoint.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace DevTask1
+{
+    class EntryPoint
+    {
+        /// <summary>
+        /// Program accepts arguments from the command line and displays
+        /// to the console all sequences without consecutive repetitive symbols
+        /// </summary>
+        /// <param name="args">The command line arguments</param>
+        static void Main(string[] args)
+        {
+            if (args.Length == 0 || args[0].Length < 2)
+            {
+                Console.WriteLine("Too little muber of arguments");
+            }
+            else
+            {
+                SequencesFinder sequencesFinder = new SequencesFinder(args);
+                sequencesFinder.FindSequences();
+            }
+        }
+    }
+}
---
+++ DevTask1/DevTask1/SequencesFinder.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Text;
+
+namespace DevTask1
+{
+    class SequencesFinder
+    {
+        StringBuilder processedString;
+
+        /// <summary>
+        /// Constructor forms a string from array of strings
+        /// </summary>
+        /// <param name="inputString">Array of strings</param>
+        public SequencesFinder(string[] inputString)
+        {
+            processedString = new StringBuilder();
+            foreach (string word in inputString)
+            {
+                processedString.Append(word).Append(" ");
+            }
+            processedString.Remove(processedString.Length - 1, 1);
+        }
+
+        /// <summary>
+        /// Сompares current symbol with next symbols
+        /// and displays it if they different
+        /// </summary>
+        public void FindSequences()
+        {
+            StringBuilder sequence;
+            for (int i = 0; i < processedString.Length - 1; i++)
+            {
+                sequence = new StringBuilder();
+                sequence.Append(processedString[i]);
+                for (int j = i + 1; j < processedString.Length; j++)
+                {
+                    if (processedString[j - 1] != processedString[j])
+                    {
+                        sequence.Append(processedString[j]);
+                        Console.WriteLine(sequence);
+                    }
+                    else break;
+                }
+            }
+        }
+    }
+}
